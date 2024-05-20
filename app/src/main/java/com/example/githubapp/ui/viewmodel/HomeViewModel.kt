5516d9b7917ed1da21d42a449b6b1b6e4654ff7e
Moirand <andreansyah1815@gmail.com>
2024-05-20 17:43:40 +0700

    Menyesuaikan HomeScreen dan menambahkan fitur delete akun dan sign out

--- app/src/main/java/com/example/githubapp/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/githubapp/ui/viewmodel/HomeViewModel.kt
@@ -8,37 +8,32 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.githubapp.data.domain.AuthRepository
 import com.example.githubapp.data.domain.SettingsPreferences
 import com.example.githubapp.data.remote.response.ErrorResponse
 import com.example.githubapp.data.remote.response.UserResponseItem
 import com.example.githubapp.data.remote.retrofit.ApiConfig
 import com.example.githubapp.di.Injection
 import com.google.gson.Gson
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
-class HomeViewModel(private val preferences: SettingsPreferences) : ViewModel() {
+class HomeViewModel(
+    private val authRepository: AuthRepository,
+    private val preferences: SettingsPreferences
+) : ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
-    private val _error = MutableLiveData<String?>()
-    val error: LiveData<String?> = _error
+    private val _error = MutableLiveData<Exception>()
+    val error: LiveData<Exception> = _error
 
     private val _listUsers = MutableLiveData<List<UserResponseItem>?>()
     val listUsers: LiveData<List<UserResponseItem>?> = _listUsers
 
-    private val _getMode = MutableLiveData<Boolean>()
-    val getMode: LiveData<Boolean> = _getMode
-
-    fun getMode() {
-        viewModelScope.launch {
-            preferences.getMode().collect {
-                _getMode.value = it
-            }
-        }
-    }
-
-    fun getAllUsers(context: Context, username: String? = null) {
+    fun getUsers(context: Context, username: String? = null) {
         viewModelScope.launch {
             try {
                 _isLoading.value = true
@@ -52,29 +47,59 @@ class HomeViewModel(private val preferences: SettingsPreferences) : ViewModel() 
                 if (e is HttpException) {
                     val json = e.response()?.errorBody()?.string()
                     val error = Gson().fromJson(json, ErrorResponse::class.java)
-                    _error.value = error.message
+                    _error.value = Exception(error.message)
                 } else {
-                    _error.value = e.message
+                    _error.value = e
                 }
+                _isLoading.value = false
+            }
+        }
+    }
+
+    fun deleteAccount() {
+        viewModelScope.launch {
+            try {
+                preferences.loadUserId().collect {
+                    authRepository.deleteUser(it)
+                }
+            } catch (e: Exception) {
+                _error.value = e
+            }
+        }
+    }
+
+    fun signOut(): Deferred<Unit> {
+        return viewModelScope.async {
+            try {
+                preferences.deleteToken()
+                preferences.deleteUserId()
+            } catch (e: Exception) {
+                _error.value = e
             }
         }
     }
 }
 
-class HomeViewModelFactory(private val pref: SettingsPreferences) :
+class HomeViewModelFactory(
+    private val authRepository: AuthRepository,
+    private val pref: SettingsPreferences
+) :
     ViewModelProvider.NewInstanceFactory() {
     companion object {
         @Volatile
         private var instance: HomeViewModelFactory? = null
-        fun getInstance(datastore: DataStore<Preferences>): HomeViewModelFactory =
+        fun getInstance(context: Context, datastore: DataStore<Preferences>): HomeViewModelFactory =
             instance ?: synchronized(this) {
                 instance
-                    ?: HomeViewModelFactory(Injection.provideSettingsPreferences(datastore))
+                    ?: HomeViewModelFactory(
+                        Injection.provideAuthRepository(context),
+                        Injection.provideSettingsPreferences(datastore)
+                    )
             }.also { instance = it }
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return HomeViewModel(pref) as T
+        return HomeViewModel(authRepository, pref) as T
     }
 }
