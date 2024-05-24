b51f536f61d1066d504414cf5506b263c16d27b0
Moirand <andreansyah1815@gmail.com>
2024-05-24 16:03:36 +0700

    Membuat halaman favorit

--- app/src/main/java/com/example/githubapp/ui/utill/Utill.kt
+++ app/src/main/java/com/example/githubapp/ui/utill/Utill.kt
@@ -2,6 +2,8 @@ package com.example.githubapp.ui.utill
 
 import android.content.Context
 import com.bumptech.glide.Glide
+import com.example.githubapp.data.remote.response.DetailUserResponse
+import com.example.githubapp.data.remote.response.UserResponseItem
 import com.google.android.material.imageview.ShapeableImageView
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -18,4 +20,32 @@ fun String.toDate(): String {
     val outputFormat = SimpleDateFormat("d MMM yyyy", Locale.getDefault())
 
     return outputFormat.format(inputFormat.parse(this))
+}
+
+fun generateToken(): String {
+    val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    return (1..20).map { allowedChars.random() }.joinToString("")
+}
+
+fun DetailUserResponse.toUserResponseItem(): UserResponseItem{
+    return UserResponseItem(
+        gistsUrl = this.gistsUrl,
+        reposUrl = this.reposUrl,
+        followingUrl = this.followingUrl,
+        starredUrl = this.starredUrl,
+        login = this.login,
+        followersUrl = this.followersUrl,
+        type = this.type,
+        url = this.url,
+        subscriptionsUrl = this.subscriptionsUrl,
+        receivedEventsUrl = this.receivedEventsUrl,
+        avatarUrl = this.avatarUrl,
+        eventsUrl = this.eventsUrl,
+        htmlUrl = this.htmlUrl,
+        siteAdmin = this.siteAdmin,
+        id = this.id,
+        gravatarId = this.gravatarId,
+        nodeId = this.nodeId,
+        organizationsUrl = this.organizationsUrl
+    )
 }
---
+++ app/src/main/java/com/example/githubapp/ui/viewmodel/FavoriteViewModel.kt
@@ -0,0 +1,91 @@
+package com.example.githubapp.ui.viewmodel
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.githubapp.data.domain.SettingsPreferences
+import com.example.githubapp.data.domain.UserRepository
+import com.example.githubapp.data.remote.response.ErrorResponse
+import com.example.githubapp.data.remote.response.UserResponseItem
+import com.example.githubapp.data.remote.retrofit.ApiConfig
+import com.example.githubapp.di.Injection
+import com.example.githubapp.ui.utill.toUserResponseItem
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+
+class FavoriteViewModel(
+    private val userRepository: UserRepository,
+    private val preferences: SettingsPreferences
+) : ViewModel() {
+    private var _userId = -1
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _error = MutableLiveData<Exception>()
+    val error: LiveData<Exception> = _error
+
+    private val _listUsers: MutableList<UserResponseItem> = mutableListOf()
+    val listUsers: LiveData<List<UserResponseItem>> = MutableLiveData(_listUsers)
+
+    fun getUsers(context: Context) {
+        viewModelScope.launch {
+            try {
+                preferences.loadUserId().collect { id ->
+                    _isLoading.value = true
+                    _userId = id
+                    val listFavorite = userRepository.getFavoriteList(_userId)
+                    listFavorite.forEach {
+                        _listUsers.add(
+                            ApiConfig.provideApiService(context).getDetailUser(it)
+                                .toUserResponseItem()
+                        )
+                    }
+                    _isLoading.value = false
+                }
+            } catch (e: Exception) {
+                if (e is HttpException) {
+                    val json = e.response()?.errorBody()?.string()
+                    val error = Gson().fromJson(json, ErrorResponse::class.java)
+                    _error.value = Exception(error.message)
+                } else {
+                    _error.value = e
+                }
+                _isLoading.value = false
+            }
+        }
+    }
+}
+
+class FavoriteViewModelFactory(
+    private val userRepository: UserRepository,
+    private val preferences: SettingsPreferences
+) :
+    ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        @Volatile
+        private var instance: FavoriteViewModelFactory? = null
+        fun getInstance(
+            context: Context,
+            datastore: DataStore<Preferences>
+        ): FavoriteViewModelFactory =
+            instance ?: synchronized(this) {
+                instance
+                    ?: FavoriteViewModelFactory(
+                        Injection.provideUserRepository(context),
+                        Injection.provideSettingsPreferences(datastore)
+                    )
+            }.also { instance = it }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return FavoriteViewModel(userRepository, preferences) as T
+    }
+}
