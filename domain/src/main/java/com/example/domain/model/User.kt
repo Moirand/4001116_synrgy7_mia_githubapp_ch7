2f4a0bfdc2fa57ab40413183d3f801b056eba611
Moirand <andreansyah1815@gmail.com>
2024-05-27 14:19:48 +0700

    Membuat model User.kt

---
+++ domain/src/main/java/com/example/domain/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.domain.model
+
+data class User(
+    val userId: Int,
+    val username: String,
+    val email: String,
+    val password: String,
+    val favoriteList: String,
+)
